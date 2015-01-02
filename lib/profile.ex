8250cd913d47440a20d734894b8e7de44fced023
Joseph Kain <joekain@gmail.com>
2015-01-02 14:24:54 -0800

    Switch to fprof for more detail.

--- lib/profile.ex
+++ lib/profile.ex
@@ -2,8 +2,10 @@ defmodule Profile do
   import ExProf.Macro
 
   def go do
-    profile do
-      Life.CLI.main(["--iterations", "100", "--seed", "test_data/pulsar1.dat"])
-    end
+    :fprof.apply(&run_test/0, [])
+    :fprof.profile()
+    :fprof.analyse()
   end 
+  
+  defp run_test, do: Life.CLI.main(["--animated", "--iterations", "5", "--seed", "test_data/glider1.dat"])
 end
