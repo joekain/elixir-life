7ada78e01d01bb4f525434e74051304495ae21ae
Joseph Kain <joekain@gmail.com>
2015-01-01 23:20:02 -0800

    Implement rules 1 and 2 and route through tick.
    All tests are now passing.

--- lib/life.ex
+++ lib/life.ex
@@ -1,5 +1,8 @@
 defmodule Life do
   alias Life.Board
+  
+  @live 79  # O
+  @dead 32  # <space>
 
   def seed_board({args, _, _}) do
     Keyword.get(args, :seed) |> do_seed_board
@@ -12,8 +15,35 @@ defmodule Life do
   
   defp empty_board, do: do_seed_board("test_data/empty10x10.dat")
 
-  def tick(_board) do
-    empty_board
+  def tick(board) do
+    board |> Board.map(fn (key, value) -> apply_rules(key, value, board) end)
+  end
+  
+  defp apply_rules({x, y}, value, board) do
+    case {value, count_live_neighbors(x, y, board)} do
+      {v, c} when (v == @live) and (c < 2)            -> @dead
+      {v, c} when (v == @live) and (c == 2 or c == 3) -> @live
+      {v, _}                                          -> v
+    end
+  end
+  
+  defp count_live_neighbors(x, y, board) do
+    list_of_neighbors(x, y, board)
+    |> Enum.map(&state_as_int/1)
+    |> Enum.sum
+  end
+  
+  defp list_of_neighbors(x, y, board) do
+    for dx <- [-1, 0, 1], dy <- [-1, 0, 1], {dx, dy} != {0, 0} do
+      board[{x + dx, y + dy}]
+    end
+  end
+  
+  defp state_as_int(state) do
+    case state do
+      @live -> 1
+      _     -> 0
+    end
   end
   
   def to_string(board), do: Board.to_string(board)
