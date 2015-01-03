5fe4f1b3b04bd8e2bce84d5aa1965814db030ff9
Joseph Kain <joekain@gmail.com>
2015-01-03 12:18:13 -0800

    Enable eflame as profiler.

--- lib/profile.ex
+++ lib/profile.ex
@@ -2,16 +2,7 @@ defmodule Profile do
   import ExProf.Macro
 
   def go do
-    :fprof.apply(&run_test/0, [])
-    :fprof.profile()
-    :fprof.analyse(
-      [
-        callers: true,
-        sort: :own,
-        totals: true,
-        details: true
-      ]
-    )
+    :eflame.apply(&run_test/0, [])
   end 
   
   def run_test, do: Life.CLI.main(["--animated", "--iterations", "5", "--seed", "test_data/glider1.dat"])
