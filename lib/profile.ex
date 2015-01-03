0dd941ce32764bdc319c00be6ecb381152e5e370
Joseph Kain <joekain@gmail.com>
2015-01-03 12:39:27 -0800

    Add functions for each profiler (eflame, fprof, eprof)

--- lib/profile.ex
+++ lib/profile.ex
@@ -1,9 +1,25 @@
 defmodule Profile do
   import ExProf.Macro
 
-  def go do
+  def eflame do
     :eflame.apply(&run_test/0, [])
   end 
   
+  def fprof do
+    :fprof.apply(&run_test/0, [])
+    :fprof.profile()
+    :fprof.analyse(
+      [
+        callers: true,
+        sort: :own,
+        totals: true,
+        details: true
+      ]
+    )
+  end
+  
+  def eprof, do:
+    profile do: run_test
+
   def run_test, do: Life.CLI.main(["--animated", "--iterations", "5", "--seed", "test_data/glider1.dat"])
 end
