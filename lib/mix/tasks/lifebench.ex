b870e5023bb303bf3553067b4b527fcb32c5b67d
Joseph Kain <joekain@gmail.com>
2015-05-31 09:50:05 -0700

    Add type specs for no_return
    Add type specs to usage functions which call Kernel.exit/1 and
    never return.
    
    Fixes dialyzer errors.

--- lib/mix/tasks/lifebench.ex
+++ lib/mix/tasks/lifebench.ex
@@ -1,38 +1,38 @@
 defmodule Mix.Tasks.Lifebench do
   use Mix.Task
-  
+
   @shortdoc "Benchmark the Game of Life"
 
   @moduledoc """
   Run a benchmark and save results to a results file.
-  
+
   Usage: mix lifebench <results file>
   """
-  
+
   @number_of_runs 10
-  
+
   def run(args) do
     prepare
-    
+
     collect_results(&Profile.run_test/0)
     |> write_results(args)
   end
-  
+
   defp prepare do
     Mix.Project.get!
     Mix.Task.run("compile", [])
     Mix.Task.run("loadpaths", [])
   end
-  
+
   def collect_results(f) do
     Stream.repeatedly(fn -> time(f) end) |> Stream.take(@number_of_runs)
   end
-  
+
   defp time(f) do
     {time, _value} = :timer.tc(f)
     time
   end
-  
+
   defp write_results(results, args) do
     f = get_output_file(args)
 
@@ -40,21 +40,22 @@ defmodule Mix.Tasks.Lifebench do
     |> Enum.to_list
     |> Enum.map(fn (result) -> IO.puts(f, "#{result}") end)
   end
-  
+
   defp get_output_file(args) do
     case OptionParser.parse(args, strict: []) do
       {[], [name], []} -> open_output_file(name)
       {_, _, _}        -> usage
     end
   end
-  
+
   defp open_output_file(name) do
     case File.open(name, [:write]) do
       {:ok, file} -> file
       _           -> usage
     end
   end
-  
+
+  @spec usage :: no_return
   defp usage do
     Kernel.exit("Usage: mix lifebench <results file>")
   end
--- lib/mix/tasks/lifebench_cmp.ex
+++ lib/mix/tasks/lifebench_cmp.ex
@@ -1,24 +1,24 @@
 defmodule Mix.Tasks.Lifebench.Cmp do
   use Mix.Task
-  
+
   defmodule Stats do
     defstruct count: 0, mean: 0, stdev: 0
   end
-  
+
   @shortdoc "Compare Benchmarks of the Game of Life"
 
   @moduledoc """
   Compare results from previous lifebench runs.
-  
+
   Usage: mix lifebench <results file 1> <results file 2>
   """
-  
+
   def run(args) do
     args
     |> parse_args
     |> compare
   end
-  
+
   defp compare(files) do
     files
     |> Enum.map(&load_results(&1))
@@ -26,14 +26,14 @@ defmodule Mix.Tasks.Lifebench.Cmp do
     |> compute_t_value
     |> report
   end
-  
+
   defp load_results(filename) do
-    File.stream!(filename) 
-    |> Enum.to_list 
+    File.stream!(filename)
+    |> Enum.to_list
     |> Enum.map(&String.strip(&1))
     |> Enum.map(&String.to_integer(&1))
   end
-  
+
   defp compute_stats(results) do
     %Stats
     {
@@ -49,43 +49,43 @@ defmodule Mix.Tasks.Lifebench.Cmp do
     t = compute_t(a, b, stats)
     {stats, t}
   end
-  
+
   defp compute_a([%Stats{count: n1}, %Stats{count: n2}]) do
-    (n1 + n2) / (n1 * n2) 
+    (n1 + n2) / (n1 * n2)
   end
-  
+
   defp compute_b([%Stats{count: n1, stdev: s1}, %Stats{count: n2, stdev: s2}]) do
-    ( ((n1 - 1) * s1 * s1) + ((n2 - 1) * s2 * s2) ) / (n1 + n2 - 2) 
+    ( ((n1 - 1) * s1 * s1) + ((n2 - 1) * s2 * s2) ) / (n1 + n2 - 2)
   end
-  
+
   defp compute_t(a, b, [%Stats{mean: u1}, %Stats{mean: u2}]) do
     abs(u1 - u2) / :math.sqrt(a * b)
   end
-  
+
   defp report({[%Stats{mean: u1}, %Stats{mean: u2}] = stats, t}) do
     IO.puts "#{u1} -> #{u2} (#{percent_increase(u1, u2)}) with p < #{t_dist(t, df(stats))}"
     IO.puts "t = #{t}, #{df(stats)} degrees of freedom"
   end
-  
+
   defp corrected_sample_stdev(list) do
     mean = Statistics.mean(list)
     (Enum.map(list, fn(x) -> (mean - x) * (mean - x) end) |> Enum.sum) / (Enum.count(list) - 1)
     |> :math.sqrt
   end
-  
+
   defp t_dist(t, df) do
     Distribution.t(t, df)
   end
-  
+
   # This seems to be inaccurate, for example it returns 1.0044 for T.cdf(3.057950289696126, 18)
   # defp t_dist(t, df) do
   #  1 - Statistics.Distributions.T.cdf(t, df)
   #end
-  
+
   defp df([%Stats{count: n1}, %Stats{count: n2}]) do
     n1 + n2 - 2
   end
-  
+
   def percent_increase(u1, u2) do
     percent = 100 * (u2 - u1) / u1
     percent_s = Float.to_string(percent, [decimals: 2])
@@ -94,14 +94,15 @@ defmodule Mix.Tasks.Lifebench.Cmp do
       true        -> "+#{percent_s}%"
     end
   end
-    
+
   defp parse_args(args) do
     case OptionParser.parse(args, strict: []) do
       {[], [name1, name2], []} -> [name1, name2]
       {_, _, _}                -> usage
     end
   end
-  
+
+  @spec usage :: no_return
   defp usage do
     Kernel.exit("Usage: mix lifebench.cmp <results file 1> <results file 2>")
   end
