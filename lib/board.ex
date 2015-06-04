4d733ad695fa88a97c92fa5cdeb17af58387fd2a
Joseph Kain <joekain@gmail.com>
2015-06-04 00:04:36 -0700

    Add some type annotations.

--- lib/board.ex
+++ lib/board.ex
@@ -1,20 +1,26 @@
 defmodule Life.Board do
+
+  @type t :: map
+
+  @spec new_from_file(Path.t) :: t
   def new_from_file(seedfile) do
     {:ok, seed} = File.read(seedfile)
     new_from_string(seed)
   end
-  
+
+  @spec new_from_string(String.t) :: t
   def new_from_string(string) do
     Map.new |> insert(0, 0, String.to_char_list(string))
   end
-  
+
+  @spec insert(map, integer(), integer(),char_list()) :: map
   defp insert(map, _x, _y, []), do: map
-  defp insert(map, _x,  y, [10 | list]), do: insert(map, 0, y + 1, list)  
+  defp insert(map, _x,  y, [10 | list]), do: insert(map, 0, y + 1, list)
   defp insert(map,  x,  y, [char | list]) do
     new_map = Map.put(map, {x, y}, char)
     insert(new_map, x + 1, y, list)
   end
-  
+
   def to_string(board) do
     board
     |> Map.keys
@@ -22,20 +28,20 @@ defmodule Life.Board do
     |> sorted_map_to_char_list(board)
     |> List.to_string
   end
-  
+
   defp coordinate_preceeds?({x1, y1}, {x2, y2}) do
-    cond do      
+    cond do
       (y1 < y2) ->               true
       (y1 == y2) && (x1 < x2) -> true
       true ->                    false
     end
   end
-  
+
   defp sorted_map_to_char_list(sorted_list, board) do
     current_y = 0
     do_transform(sorted_list, board, current_y)
   end
-  
+
   defp do_transform([], _board, _current_y), do: [10]
   defp do_transform([{x,y} | tail], board, current_y) do
     cond do
@@ -43,23 +49,23 @@ defmodule Life.Board do
       true           -> [ 10, board[{x,y}] | do_transform(tail, board, y) ]
     end
   end
-  
+
   def map(board, f) do
     board
     |> Map.keys
     |> pmap(fn (key) -> { key, f.(key, board[key]) } end)
     |> List.foldr(Map.new, fn ({key, value}, acc) -> Map.put(acc, key, value) end)
   end
-  
+
   @chunk_size 128
-  
+
   def pmap(list, f) do
     list
     |> Enum.chunk(@chunk_size, @chunk_size, [])
     |> Enum.map(&async_map(&1, f))
     |> Enum.flat_map(&async_map_results(&1))
   end
-  
+
   def async_map(list, f), do: Task.async(fn -> Enum.map(list, f) end)
   def async_map_results(async), do: Task.await(async)
 end
--- lib/life.ex
+++ lib/life.ex
@@ -1,9 +1,9 @@
 defmodule Life do
   alias Life.Board
-  
+
   @live 79  # O
   @dead 32  # <space>
-  
+
   def run_game(args, display \\ &IO.write/1) do
     args
     |> seed_board
@@ -16,10 +16,10 @@ defmodule Life do
   defp do_seed_board(seedfile), do: Board.new_from_file(seedfile)
 
   defp empty_board, do: do_seed_board("test_data/empty10x10.dat")
-  
+
   defp iterations({args, _, _}), do: Keyword.get(args, :iterations)
   defp animated?({args, _, _}),  do: Keyword.get(args, :animated)
-  
+
   defp run_loop(board, 0, _display), do: board
   defp run_loop(board, nil, _display), do: board
   defp run_loop(board, count, display) do
@@ -31,7 +31,8 @@ defmodule Life do
 
   defp tick(board), do:
     board |> Board.map(fn (key, value) -> apply_rules(key, value, board) end)
-  
+
+  @spec apply_rules({Integer, Integer}, char(), Board.t) :: Integer
   defp apply_rules({x, y}, value, board) do
     case {value, count_live_neighbors(x, y, board)} do
       {v, c} when (v == @live) and (c < 2)            -> @dead
@@ -41,20 +42,22 @@ defmodule Life do
       {v, _}                                          -> v
     end
   end
-  
+
+  @spec count_live_neighbors(Integer, Integer, any) :: integer()
   defp count_live_neighbors(x, y, board) do
     list_of_neighbors(x, y, board)
     |> Enum.map(&state_as_int/1)
     |> Enum.sum
+    |> round
   end
-  
+
   defp list_of_neighbors(x, y, board) do
     for dx <- [-1, 0, 1], dy <- [-1, 0, 1], {dx, dy} != {0, 0}, do: board[{x + dx, y + dy}]
   end
 
   defp state_as_int(@live),                           do: 1
   defp state_as_int(_dead_or_nil_when_out_of_bounds), do: 0
-  
+
   defp output(board, display, enable \\ true) do
     if enable do
       board
@@ -64,7 +67,8 @@ defmodule Life do
 
     board
   end
-  
+
+  @spec board_to_string(Board.t) :: String.t
   def board_to_string(board), do: Board.to_string(board)
 
 end
